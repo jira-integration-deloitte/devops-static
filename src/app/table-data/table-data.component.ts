7960f700691141cf7291569a0cfb30055c19dd5c
US\arnamondal <arnamondal@deloitte.com>
2018-09-19 15:26:00 +0530

    Total row updated

--- src/app/table-data/table-data.component.ts
+++ src/app/table-data/table-data.component.ts
@@ -8,32 +8,49 @@ declare var $;
   styleUrls: ['./table-data.component.css']
 })
 export class TableDataComponent {
-    constructor(private service: DevopsHttpService){}
+    constructor(private service: DevopsHttpService) {}
     statusColumnCount: number;
     groomingStatusColumnCount: number;
     responseData: any;
     showTable = false;
+    currentStatus = '';
+
     loadTable(boardId, testOnly) {
-        $('#board-summary-table').DataTable();
+        this.currentStatus = 'Fetching sprints'
+        this.showLoader();
+
         console.log('Loading summary - ' + boardId);
         const url = Constants.HOST + '/devops-service/board/' + boardId + '/sprintsDetails' + (testOnly ? '?testOnly=true' : '');
         const data = this.service.getData(url);
         data.subscribe((response: any) => {
-
-           console.log(response);
-           this.statusColumnCount = response.statusColCount;
-           this.groomingStatusColumnCount = response.groomingStatusColCount;
-           this.responseData = response;
-
+            console.log(response);
+            this.statusColumnCount = response.statusColCount;
+            this.groomingStatusColumnCount = response.groomingStatusColCount;
+            this.responseData = response;
+                console.log('Response value assigned');
             this.showTable = true;
+            console.log('Showtable value updated');
+            this.currentStatus = 'Done : All stories have been fetched';
+            this.hideLoader();
         },
             (error: any) => {
             console.log(error);
+            this.currentStatus = 'Error : Unable to fetch';
+            this.hideLoader();
         },
             () => function() {
 
             }
         );
+       // $('#board-summary-table').DataTable();
+    }
 
+    showLoader() {
+        console.log('Showing loader - ' + this.currentStatus);
+        $('#show-loader-btn').click();
+    }
+    hideLoader() {
+        console.log('Hiding loader - ' + this.currentStatus);
+        $('#hide-loader-btn').click();
     }
 }
